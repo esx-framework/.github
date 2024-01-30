387642a4b632f247d35ae436f02117cae87e48d3
Gellipapa <gellerthorvath0@gmail.com>
2024-01-30 01:38:43 +0100

    Update bump-manifest-version.js

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -47,6 +47,7 @@ replaceInFiles(defaultFolderPath);
 process.on('exit', () => {
   const changedFiles = allResults.map(result => result.file);
     console.log(changedFiles)
+    console.log(defaultFolderPath)
   if (changedFiles.length > 0) {
     console.log(`echo "FXMANIFEST_FILE_CHANGES=${JSON.stringify(changedFiles)}" >> $GITHUB_ENV`);
   } else {
