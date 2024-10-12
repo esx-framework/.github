631413c6728fb788b11273e7aa28db98097e7b38
Gellipapa <gellerthorvath0@gmail.com>
2024-10-12 22:40:28 +0200

    Create bump-web-version.js

---
+++ .github/actions/bump-web-version.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require('path');
+const replace = require('replace-in-file');
+
+const version = process.env.TGT_RELEASE_VERSION;
+const newVersion = version.replace("v", "");
+
+const results = replace.sync({
+    files: "fxmanifest.lua",
+    from: /\bversion\s+(.*)$/gm,
+    to: `version '${newVersion}'`,
+});
+
+console.log(results)
